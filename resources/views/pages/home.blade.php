7115e78f72d73287cb7e3425592c1d92e4bfab26
lineabbening-pixel <lineabbening@gmail.com>
2025-10-25 03:10:48 +0700

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="text-center py-5">
-    <h1>Welcome to My Portfolio</h1>
+    <h1>Welcome to My CV</h1>
     <p class="lead">Click below to view my CV</p>
     <a href="{{ route('cv.index') }}" class="btn btn-primary mt-3">View CV</a>
 </div>
